294be79eab712558a49ce31318bf0d73ee65205e
Bjørn Kristian Larsen <bjornkl@met.no>
2021-02-04 01:53:50 +0100

    added missing files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Color, MuiThemeProvider, SimplePaletteColorOptions } from '@material-ui/core';
+import { MuiThemeProvider, SimplePaletteColorOptions } from '@material-ui/core';
 import {
   makeStyles,
   createStyles,
